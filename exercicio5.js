646b29d481ddd82d99e415486a5f51abb0735a5c
Jonathan da Cruz <jonathanadcz@gmail.com>
2022-03-22 13:07:40 -0300

    Update exercicio5.js

--- exercicio5.js
+++ exercicio5.js
@@ -1,1 +1,20 @@
 
+function primeNumbers(num) {
+  let numbers = new Array();
+  for (var i = 0; i <= num; i++) {
+    if (isPrime(i)){
+      numbers.push(i);
+    }
+  }
+  return numbers;
+}
+function isPrime(num) {
+  for(let i = 2; i <num; i++)
+    if(num % i === 0) {
+        return false
+    };
+  return num > 1;
+}
+
+
+console.log('Os números primos de 0 - 20 são : '+ primeNumbers(20));
